8d0dbec803e12175bf4d8dab7cdfae9cb1d7fea3
JIAYU LIN <louis70109@gmail.com>
2019-11-11 23:25:36 +0800

    integration sentry

--- api.py
+++ api.py
@@ -1,22 +1,43 @@
+from http.client import HTTPException
 from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import TextSendMessage
 import json
-from linebot import (
-    LineBotApi, WebhookHandler
+import os
+import sentry_sdk
+import logging
+from sentry_sdk.integrations.flask import FlaskIntegration
+from sentry_sdk.integrations.logging import LoggingIntegration
+
+
+def strip_sensitive_data(event, hint):
+    if "exc_info" in hint:
+        instance = hint['exc_info'][1]
+        if isinstance(instance, HTTPException) and instance.code < 500:
+            return None
+    return event
+
+
+sentry_logging = LoggingIntegration(
+    level=logging.INFO,  # Capture info and above as breadcrumbs
+    event_level=logging.WARNING  # Send errors as events
 )
-from linebot.exceptions import (
-    InvalidSignatureError
+
+sentry_sdk.init(
+    dsn=os.environ.get('SENTRY_DSN'),
+    before_send=strip_sensitive_data,
+    integrations=[FlaskIntegration(), sentry_logging]
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+
 
 app = Flask(__name__)
 
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
-    line_bot_api = LineBotApi('YOUR_CHANNEL_TOKEN')
-    handler = WebhookHandler('YOUR_CHANNEL_SECRET_KEY')
+    line_bot_api = LineBotApi(os.environ.get('LINE_CHANNEL_TOKEN'))
+    handler = WebhookHandler(os.environ.get('LINE_CHANNEL_SECRET_KEY'))
 
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
@@ -36,7 +57,7 @@ def webhook():
         pass
     else:
         line_bot_api.reply_message(token, TextSendMessage(
-            text=event['events'][0]['message']['text']))
+                text=event['events'][0]['message']['text']))
     return 'OK'
 
 
